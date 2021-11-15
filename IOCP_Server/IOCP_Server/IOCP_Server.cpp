e614012aa296e0eb5b68a2a407b4eee3e7b73d6d
fanjaekim <hjjang45@gmail.com>
2021-11-15 17:36:13 +0900

    C++ IOCP Echo Server

---
+++ IOCP_Server/IOCP_Server/IOCP_Server.cpp
@@ -0,0 +1,125 @@
+﻿#include "pch.h"
+#include <stdio.h>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define MAX_BUFFER 1024
+#define SERVER_PORT 3500
+
+struct SOCKETINFO
+{
+	WSAOVERLAPPED overlapped;
+	WSABUF dataBuffer;
+	SOCKET socket;
+	char messageBuffer[MAX_BUFFER];
+	int receiveBytes;
+	int sendBytes;
+};
+
+DWORD WINAPI makeThread(LPVOID hIOCP);
+int main(int argc, char* argv[])
+{
+	// Winsock Start - windock.dll load
+	WSADATA WSAData;
+	if (WSAStartup(MAKEWORD(2, 2), &WSAData) != 0)
+	{
+		printf("Error - Cannot load 'winsock.dll' file\n");
+		return 1;
+	}
+
+	// 1. socket 생성
+	SOCKET listenSocket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		printf("Error - Invalid socket\n");
+		return 1;
+	}
+
+	// 서버정보 객체설정
+	SOCKADDR_IN serverAddr;
+	memset(&serverAddr, 0, sizeof(SOCKADDR_IN));
+	serverAddr.sin_family = PF_INET;
+	serverAddr.sin_port = htons(SERVER_PORT);
+	serverAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+
+	// 2. 소켓 설정
+	if (bind(listenSocket, (struct sockaddr*)&serverAddr, sizeof(SOCKADDR_IN)) == SOCKET_ERROR)
+	{
+		printf("Error - Fail bind\n");
+		// 6.  소켓 종료
+		closesocket(listenSocket);
+		// Winsock end
+		WSACleanup();
+		return 1;
+	}
+
+	// 완료 결과를 처리하는 객체(CP : Completion Port) 생성
+	HANDLE hIOCP = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+
+	// WorkerThread 생성
+	// CPU * 2개 만큼 생성
+	SYSTEM_INFO systemInfo;
+	GetSystemInfo(&systemInfo);
+	int threadCount = systemInfo.dwNumberOfProcessors * 2;
+	unsigned long threadId;
+
+	// thread Handler 선언
+	HANDLE *hThread = (HANDLE *)malloc(threadCount * sizeof(HANDLE));
+
+	// thread 생성
+	for (int i = 0; i < threadCount; i++)
+	{
+		hThread[i] = CreateThread(NULL, 0, makeThread, &hIOCP, 0, &threadId);
+	}
+
+	SOCKADDR_IN clientAddr;
+	int addrLen = sizeof(SOCKADDR_IN);
+	memset(&clientAddr, 0, addrLen);
+	SOCKET clientSocket;
+	SOCKETINFO *socketInfo;
+	DWORD receiveBytes;
+	DWORD flags;
+
+	while (1)
+	{
+		clientSocket = accept(listenSocket, (struct sockaddr*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			printf("Error - Accept Failure\n");
+			return 1;
+		}
+
+		socketInfo = (struct SOCKETINFO *)malloc(sizeof(struct SOCKETINFO));
+		memset((void *)socketInfo, 0x00, sizeof(struct SOCKETINFO));
+		socketInfo->socket = clientSocket;
+		socketInfo->receiveBytes = 0;
+		socketInfo->sendBytes = 0;
+		socketInfo->dataBuffer.len = MAX_BUFFER;
+		socketInfo->dataBuffer.buf = socketInfo->messageBuffer;
+		flags = 0;
+
+		hIOCP = CreateIoCompletionPort((HANDLE)clientSocket, hIOCP, (DWORD)socketInfo, 0);
+
+		// 중첩 소켓을 지정하고 완료시 실행될 함수를 넘겨준다.
+		if (WSARecv(socketInfo->socket, &socketInfo->dataBuffer, 1, &receiveBytes, &flags, &(socketInfo->overlapped), NULL))
+		{
+			if (WSAGetLastError() != WSA_IO_PENDING)
+			{
+				printf("Error - IO pending Failure\n");
+				return 1;
+			}
+		}
+	}
+
+	// 리슨 소켓 종료
+	closesocket(listenSocket);
+
+	// Winsock end
+	WSACleanup();
+
+	return 0;
+}
+
+
+
